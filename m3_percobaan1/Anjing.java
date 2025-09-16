a467743e9341a5922cf436de990cb58f91604dc9
rifkisubhanarifin-art <rifkisubhanarifin@gmail.com>
2025-09-16 21:49:32 +0700

    m3_percobaan1

---
+++ m3_percobaan1/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modul_3.m3_percobaan1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Anjing extends Hewan{
+   //Override
+    public void suara() {
+        System.out.println("Anjing menggonggong");
+    }
+}
