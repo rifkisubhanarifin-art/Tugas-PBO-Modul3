e69032b1cc26f8d2412bf916169eee2fc3411aab
rifkisubhanarifin-art <rifkisubhanarifin@gmail.com>
2025-09-16 20:50:05 +0700

    Hewan

---
+++ Hewan/Burung.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modul_3.Hewan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Burung extends Hewan {
+    //override
+    public void suara(){
+        System.out.println("Cuit! cuit!");
+    }
+}
